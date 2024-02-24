f1ad89f01f1ec4b2148bee68d546f1a7d7fde688
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-02-24 22:45:42 +0530

    updte

--- src/component/stats.js
+++ src/component/stats.js
@@ -5,7 +5,7 @@ export default function Stats() {
     <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Statistics for our Course</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">"Unlock the insights within our course data analytics hub, revealing trends and patterns to guide your educational journey. Explore our website for comprehensive statistical analysis of course offerings."</p>
     </div>
     <div className="flex flex-wrap -m-4 text-center" >
